9bc0ef3f917aef2fe50269fa99403eac146a120c
ArnavGuptas <91640989+ArnavGuptas@users.noreply.github.com>
2025-02-11 14:58:17 -0800

    Create time.py

---
+++ time.py
@@ -0,0 +1,7 @@
+#wasnt able to convert to hours :(
+from datetime import datetime
+difference = 0
+currentTime = datetime.today()
+valentineTime = datetime(2025, 2, 14, 0)
+difference = valentineTime - currentTime
+print(difference)
